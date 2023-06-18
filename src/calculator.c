a05bdee4d6feba196be942c3f661134b7b9690a5
Julus07 <123013944+Julus07@users.noreply.github.com>
2023-06-18 17:43:05 +0200

    update

--- src/calculator.c
+++ src/calculator.c
@@ -1,51 +1,57 @@
 #include <stdio.h>
+#include <windows.h>
 
-int operation;
-void chooseOperation()
+int chooseOperation()
 {
-    printf("[1] Addition | [2] Subtraktion | [3] Multiplikation | [4] Division | [5] Modulo\n");
-    printf("Choose an operation >> ");
-    scanf("%d", &operation);
-    if (operation < 1 || operation > 5)
+    int operation;
+    do
     {
-        printf("Invalid Operation\n");
-        exit(0);
-    }
+        printf("\x1B[2J\x1B[H");
+        printf("[1] Addition | [2] Subtraktion | [3] Multiplikation | [4] Division | [5] Modulo\n");
+        printf("Choose an operation >> ");
+        scanf("%d", &operation);
+        if (operation < 1 || operation > 5)
+        {
+            printf("Invalid operation! Try again.\n");
+            Sleep(1000);
+        }
+    } while (operation < 1 || operation > 5);
 }
 
-float inputNumber()
+double inputNumber()
 {
-    float input;
+    double input;
     printf("Input number >> ");
-    scanf("%f", &input);
+    scanf("%lf", &input);
     return input;
 }
 
-float berechnung(float zahl1, float zahl2)
+double result(int operation, double num1, double num2)
 {
     switch (operation)
     {
     case 1:
-        return (zahl1 + zahl2);
+        return (num1 + num2);
         break;
     case 2:
-        return (zahl1 - zahl2);
+        return (num1 - num2);
         break;
     case 3:
-        return (zahl1 * zahl2);
+        return (num1 * num2);
         break;
     case 4:
-        return (zahl1 / zahl2);
+        return (num1 / num2);
         break;
     case 5:
-        return ((int)zahl1 % (int)zahl2);
+        return ((int)num1 % (int)num2);
         break;
     }
 }
 
 int main()
 {
-    chooseOperation();
-    printf("Das Ergebnis ist: %.1f", berechnung(inputNumber(), inputNumber()));
+    int operation = chooseOperation();
+    printf("The rusult is: %.1lf", result(operation, inputNumber(), inputNumber()));
+
     return 0;
 }
