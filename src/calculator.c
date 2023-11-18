50551d298ebc2ce4fcd59e655cd726d04ca26321
Julus07 <123013944+Julus07@users.noreply.github.com>
2023-11-18 08:05:59 +0100

    add

---
+++ src/calculator.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <windows.h>
+
+int chooseOperation()
+{
+    int operation;
+    do
+    {
+        printf("\x1B[2J\x1B[H");
+        printf("[1] Addition | [2] Subtraktion | [3] Multiplikation | [4] Division | [5] Modulo\n");
+        printf("Choose an operation >> ");
+        scanf("%d", &operation);
+        if (operation < 1 || operation > 5)
+        {
+            printf("Invalid operation! Try again.\n");
+            Sleep(1000);
+        }
+    } while (operation < 1 || operation > 5);
+}
+
+double inputNumber()
+{
+    double input;
+    printf("Input number >> ");
+    scanf("%lf", &input);
+    return input;
+}
+
+double result(int operation, double num1, double num2)
+{
+    switch (operation)
+    {
+    case 1:
+        return (num1 + num2);
+        break;
+    case 2:
+        return (num1 - num2);
+        break;
+    case 3:
+        return (num1 * num2);
+        break;
+    case 4:
+        return (num1 / num2);
+        break;
+    case 5:
+        return ((int)num1 % (int)num2);
+        break;
+    }
+}
+
+int main()
+{
+    int operation = chooseOperation();
+    printf("The rusult is: %.1lf", result(operation, inputNumber(), inputNumber()));
+
+    return 0;
+}
